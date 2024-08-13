cfb24beea83cb37542b7f67d018271b2a609703e
Your GitHub Username <Your GitHub Email>
2024-08-13 16:15:57 +0800

    Create Form Submission

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,18 @@
 <?php
 
 use App\Http\Controllers\dashboardcontroller;
+use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Root Url 
-Route::get('/', [DashboardController::class, 'index']);
-
 // Sample Url for learning
 // Route::get('/profile', [ProfileController::class, 'index']);
+
+
+// Root Url 
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::post('/idea', [IdeaController::class, 'store']) ->name('idea.create');
 
 Route::get('/terms', function() {
   return view('terms');
