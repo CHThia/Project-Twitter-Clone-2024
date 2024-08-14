1bc1c62994b55c434a2c67073af323588305371a
Your GitHub Username <Your GitHub Email>
2024-08-14 17:01:22 +0800

    Validate Idea Input

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -9,6 +9,11 @@ class IdeaController extends Controller
 {
     public function store() {
 
+        // Check idea input has >= 2 chars && <= 500 chars
+        request()->validate([
+            'idea'=> 'required | min:2 | max:500'
+        ]);
+
         $idea = Idea::create (
             [
                 'content'=>request()->get('idea', ''),
