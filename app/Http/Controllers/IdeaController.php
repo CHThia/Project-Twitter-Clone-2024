00c8f09f748fff0812b33f839b7937bf143fe1cc
Your GitHub Username <Your GitHub Email>
2024-08-15 22:17:02 +0800

    Create edit feature for updating idea

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -7,33 +7,50 @@ use Illuminate\Http\Request;
 
 class IdeaController extends Controller
 {
-    public function show(Idea $idea) {
-        return view('ideas.show', compact('idea')); // compact('idea) is same as 'idea'=>$idea 
+    public function show(Idea $idea)
+    {
+        return view('ideas.show', compact('idea')); // compact('idea) is same as 'idea'=>$idea
     }
 
-
-    public function store() {
-
+    public function store()
+    {
         // Check idea input has >= 2 chars && <= 500 chars
         request()->validate([
-            'idea'=> 'required | min:2 | max:500'
+            'content' => 'required | min:2 | max:500',
         ]);
 
-        $idea = Idea::create (
-            [
-                'content'=>request()->get('idea', ''),
-            ]
-        );
-        
-        return redirect()->route('dashboard')->with('success', 'Idea created successfully.');
+        $idea = Idea::create([
+            'content' => request()->get('content', ''),
+        ]);
 
+        return redirect()->route('dashboard')->with('success', 'Idea created successfully.');
     }
 
-    public function destroy(Idea $idea) {
-        
+    public function destroy(Idea $idea)
+    {
         $idea->delete();
 
         return redirect()->route('dashboard')->with('success', 'Idea deleted successfully.');
+    }
 
+    public function edit(Idea $idea)
+    {
+        $editing = true;
+
+        return view('ideas.show', compact('idea', 'editing'));
+    }
+
+    public function update(Idea $idea)
+    {
+        request()->validate([
+            'content' => 'required | min:2 | max:500',
+        ]);
+
+        $idea->content = request()->get('content', '');
+        $idea->save();
+
+        return redirect()
+            ->route('ideas.show', $idea->id)
+            ->with('success', 'Idea updated successfully.');
     }
 }
