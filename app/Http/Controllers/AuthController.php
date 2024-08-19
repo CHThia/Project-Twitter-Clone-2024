af8c11be3e3009f5ef617680d58c003ac16ad9f9
Your GitHub Username <Your GitHub Email>
2024-08-19 16:44:29 +0800

    Create Login and Logout features

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -15,23 +16,55 @@ class AuthController extends Controller
 
     public function store()
     {
-        $validated = request()->validate(
-            [
-                'name' => 'required|min:3|max:40',
-                'email' => 'required|email|unique:users,email',
-                'password' => 'required|confirmed|min:3'
-            ]
-        );
+        $validated = request()->validate([
+            'name' => 'required|min:3|max:40',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|confirmed|min:3',
+        ]);
 
-
-        User::create(
-            [
-                'name' => $validated['name'],
-                'email' => $validated['email'],
-                'password' => Hash::make($validated['password']),
-            ]
-        );
+        User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
 
         return redirect()->route('dashboard')->with('success', 'Account created Successfully!');
+    }
+
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function authenticate()
+    {
+        // dd(request()->all());
+
+        $validated = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:3',
+        ]);
+
+        if (Auth::attempt($validated)) {
+            request()->session()->regenerate();
+
+            return redirect()->route('dashboard')->with('success', 'Logged in successfully.');
+        }
+
+        return redirect()
+            ->route('dashboard')
+            ->withErrors([
+                'email' => 'No matching user found with the provided email or password.',
+            ]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+
+        return redirect()->route('dashboard')->with('succcess', 'Logged out successfully.');
     }
 }
